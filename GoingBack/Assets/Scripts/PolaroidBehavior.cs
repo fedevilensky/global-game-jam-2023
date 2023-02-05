75de2cd9c56f57c63db31aca17ea80c4496d18ab
Nicolas <57300555+sauttize@users.noreply.github.com>
2023-02-05 08:44:58 -0300

    level 1 design complete

--- GoingBack/Assets/Scripts/PolaroidBehavior.cs
+++ GoingBack/Assets/Scripts/PolaroidBehavior.cs
@@ -5,27 +5,16 @@ using UnityEngine;
 
 public class PolaroidBehavior : MonoBehaviour
 {
-    private float alphaNumber = 255f;
-    private SpriteRenderer _spriteRenderer;
+    private Animator animator;
     void Start()
     {
-        _spriteRenderer = this.GetComponent<SpriteRenderer>();
+        animator = this.GetComponent<Animator>();
+        
         GameEvents.current.onGetsPolaroid += AlphaAnimationVoid;
-        StartCoroutine(AlphaAnimation());
     }
 
     private void AlphaAnimationVoid()
     {
-        StartCoroutine(AlphaAnimation());
-    }
-    private IEnumerator AlphaAnimation()
-    {
-        while (alphaNumber != 0)
-        {
-            alphaNumber = Mathf.Lerp(alphaNumber, 0f, .2f);
-            _spriteRenderer.color = new Color(255f, 255f, 255f, alphaNumber);
-        }
-
-        yield return null;
+        if (animator) animator.SetBool("Disappear", true);
     }
 }
