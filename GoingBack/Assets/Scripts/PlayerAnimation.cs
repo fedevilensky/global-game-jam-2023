237f390248f81f63f92d091a32647011f2e97218
Nicolas <57300555+sauttize@users.noreply.github.com>
2023-02-05 16:16:38 -0300

    game build up til level 1

--- GoingBack/Assets/Scripts/PlayerAnimation.cs
+++ GoingBack/Assets/Scripts/PlayerAnimation.cs
@@ -9,12 +9,17 @@ public class PlayerAnimation : MonoBehaviour
     private CharacterMovement movementData;
     private Rigidbody2D _rigidbody2D;
 
+    private AudioSource audioClip;
+    [SerializeField] private AudioClip walkingSound;
+    [SerializeField] private AudioClip jumpSound;
+
     void Start()
     {
         animator = this.GetComponent<Animator>();
         movementData = this.GetComponent<CharacterMovement>();
         _spriteRenderer = this.GetComponent<SpriteRenderer>();
         _rigidbody2D = this.GetComponent<Rigidbody2D>();
+        audioClip = this.GetComponent<AudioSource>();
     }
 
     void Update()
@@ -28,15 +33,39 @@ public class PlayerAnimation : MonoBehaviour
 
         if (movementData.facing == CharacterMovement.Direction.LEFT) _spriteRenderer.flipX = true;
         else _spriteRenderer.flipX = false;
-        
+
+        // Movement animation and sound
         if (movementData.isOnGround && _rigidbody2D.velocity.x != 0)
         {
             animator.SetBool("Walking", true);
-        } else animator.SetBool("Walking", false);
-        
-        if (movementData.jumpCharge > 0) animator.SetFloat("Jumping", movementData.jumpCharge);
+            if (audioClip)
+            {
+                audioClip.clip = walkingSound;
+                audioClip.loop = true;
+                audioClip.Play();
+            }
+        }
+        else
+        {
+            if (audioClip.clip == walkingSound) audioClip.Stop();
+            animator.SetBool("Walking", false);
+        }
+
+        // Jump
+        if (movementData.jumpCharge > 0)
+        {
+            if (audioClip)
+            {
+                audioClip.clip = jumpSound;
+                audioClip.loop = false;
+                audioClip.Play();
+            }
+            animator.SetFloat("Jumping", movementData.jumpCharge);
+        }
         else animator.SetFloat("Jumping", 0f);
-        
+
+
+        // Floating
         if(movementData.isOnGround == false && _rigidbody2D.velocity.y < 0) animator.SetBool("Floating", true);
         else animator.SetBool("Floating", false);
     }
--- GoingBack/Assets/Scripts/PolaroidBehavior.cs
+++ GoingBack/Assets/Scripts/PolaroidBehavior.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class PolaroidBehavior : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioClip;
     void Start()
     {
         animator = this.GetComponent<Animator>();
+        audioClip = this.GetComponent<AudioSource>();
         
         GameEvents.current.onGetsPolaroid += AlphaAnimationVoid;
     }
@@ -16,5 +18,18 @@ public class PolaroidBehavior : MonoBehaviour
     private void AlphaAnimationVoid()
     {
         if (animator) animator.SetBool("Disappear", true);
+        StartCoroutine(Destroy());
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (audioClip) audioClip.Play();
+        GameEvents.current.GetsPolaroid();
+    }
+
+    private IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(this.gameObject);
     }
 }
